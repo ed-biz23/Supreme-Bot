849aa29f25bff79d66603fda374f8c8b48390be0
Edward Biz <edbiswas@gmail.com>
2018-04-05 19:11:08 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from supreme import Supreme
+import datetime
 
-def main(category, name, color, size, checkoutDelay):
-    supreme = Supreme(category, name, color, size, checkoutDelay)
-    supreme.addToCart(supreme.search())
-    supreme.checkOut()
+def main(category, name, color, size, checkoutDelay, url):
+    if releaseType == 'R':
+        supreme = Supreme(category, name, color, size, checkoutDelay)
+        supreme.addToCart(supreme.restock(url))
+        supreme.checkOut()
+    else:
+        supreme = Supreme(category, name, color, size, checkoutDelay)
+        supreme.addToCart(supreme.search())
+        supreme.checkOut()
 
 if __name__ == '__main__':
-    category = 'jackets'        #category
-    name = ''        #item name
-    color = ''              #color
-    size = 'medium'              #size
-    checkoutDelay = None        #delay to avoid possible ghost checkout
+    # Enter R for restock mode else leave blank
+    releaseType = 'r'.upper()
+    # Enter category for release mode else leave blank
+    category = 'jackets'
+    # Enter name for release mode else leave blank
+    name = 'parka'
+    # Enter color for release mode else leave blank
+    color = 'rose'
+    # Enter size for desired size else leave blank for O/S or random
+    size = ''
+    # Enter checkout delay to avoid ghost checkout or else enter None
+    checkoutDelay = None
 
-    main(category, name, color, size, checkoutDelay) 
+    # This will be prompt if restock mode is selected else ignore this
+    url = input('Enter url: ').strip() if releaseType == 'R' else None
+
+    # Don't make any changes here
+    print(datetime.datetime.now().strftime('%x %X'), 'Starting the script')
+    main(category, name, color, size, checkoutDelay, url)
